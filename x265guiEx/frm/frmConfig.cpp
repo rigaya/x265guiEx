1e1260da6877300f4e4767ef7e61e7dd5e756247
rigaya <rigaya34589@live.jp>
2015-04-12 13:34:32 +0530

    highbit depthのチェックボックスのオンオフや設定画面の開閉などでbitrateの値が変更されてしまうことがある問題を修正。

--- x265guiEx/frm/frmConfig.cpp
+++ x265guiEx/frm/frmConfig.cpp
@@ -478,12 +478,16 @@ System::Void frmConfig::fcgTSBRearrageTabs_CheckedChanged(System::Object^  sende
 }
 
 System::Void frmConfig::fcgCBUsehighbit_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+	//一度ウィンドウの再描画を完全に抑止する
+	SendMessage(reinterpret_cast<HWND>(this->Handle.ToPointer()), WM_SETREDRAW, 0, 0);
 	fcgCXX265Mode_SelectedIndexChanged(sender, e);
 	fcgTXX265Path->Text = (fcgCBUsehighbit->Checked) ? LocalStg.x265Pathhighbit : LocalStg.x265Path;
 	fcgTXX265Path->SelectionStart = fcgTXX265Path->Text->Length;
 	fcgLBX265Path->Text = (fcgCBUsehighbit->Checked) ? L"x265.exe(highbit) の指定" : L"x265.exe の指定";
 	SetX265VersionToolTip(fcgTXX265Path->Text, fcgCBUsehighbit->Checked);
-	SetTBValueToTextBoxX265();
+	//一度ウィンドウの再描画を再開し、強制的に再描画させる
+	SendMessage(reinterpret_cast<HWND>(this->Handle.ToPointer()), WM_SETREDRAW, 1, 0);
+	this->Refresh();
 }
 
 System::Void frmConfig::fcgCBAFS_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
@@ -611,7 +615,7 @@ System::Void frmConfig::fcgCXX265Mode_SelectedIndexChanged(System::Object^  send
 			fcgCBNulOut->Checked = false;
 			fcgCBFastFirstPass->Enabled = false; //Enabledの変更が先
 			fcgCBFastFirstPass->Checked = false;
-			fcgTXQuality->Text = Convert::ToString(cnf_fcgTemp->crf / 100.0);
+			fcgTXQuality->Text = Convert::ToString(clamp(cnf_fcgTemp->crf / 100.0, (fcgCBUsehighbit->Checked) ? -12 : 0, 51));
 			SetfbcBTVBEnable(false);
 			break;
 	}
