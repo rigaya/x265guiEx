19fa8ee6c09dffa1a12823eb021abd407bac72cd
rigaya <rigaya34589@live.jp>
2018-10-08 18:57:33 +0900

    多重音声を扱う際、muxerが異常終了してしまうのを修正。 f0bb89d9 の修正では不十分だった。

--- x265guiEx/auo_version.h
+++ x265guiEx/auo_version.h
@@ -10,8 +10,8 @@
 #ifndef _AUO_VERSION_H_
 #define _AUO_VERSION_H_
 
-#define AUO_VERSION           0,3,85,0
-#define AUO_VERSION_STR       "3.85"
+#define AUO_VERSION           0,3,86,0
+#define AUO_VERSION_STR       "3.86"
 #define AUO_NAME_WITHOUT_EXT  "x265guiEx"
 #define AUO_NAME              "x265guiEx.auo"
 #define AUO_NAME_W           L"x265guiEx.auo"
