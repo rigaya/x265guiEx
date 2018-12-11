249585818595f2dee3a2c4d81e4533e4cce5bc70
rigaya <rigaya34589@live.jp>
2018-12-11 19:47:42 +0900

    Aviutlからのフレーム取得時間をログに表示するように。

--- x265guiEx/encode/auo_video.cpp
+++ x265guiEx/encode/auo_video.cpp
@@ -886,6 +886,10 @@ static AUO_RESULT x265_out(CONF_GUIEX *conf, const OUTPUT_INFO *oip, PRM_ENC *pe
         UINT64 amp_filesize_limit = (UINT64)(1.02 * get_amp_filesize_limit(conf, oip, pe, sys_dat));
         BOOL enc_pause = FALSE, copy_frame = FALSE, drop = FALSE;
         const DWORD aviutl_color_fmt = COLORFORMATS[get_aviutl_color_format(conf->x265.bit_depth, conf->x265.output_csp, conf->vid.input_as_lw48)].FOURCC;
+        double time_get_frame = 0.0;
+        int64_t qp_freq, qp_start, qp_end;
+        QueryPerformanceFrequency((LARGE_INTEGER *)&qp_freq);
+        const double qp_freq_sec = 1.0 / (double)qp_freq;
 
         //Aviutlの時間を取得
         PROCESS_TIME time_aviutl;
@@ -958,10 +962,13 @@ static AUO_RESULT x265_out(CONF_GUIEX *conf, const OUTPUT_INFO *oip, PRM_ENC *pe
             copy_frame = (!!i_frame & (oip->func_get_flag(i_frame) & OUTPUT_INFO_FRAME_FLAG_COPYFRAME));
 
             //Aviutl(afs)からフレームをもらう
+            QueryPerformanceCounter((LARGE_INTEGER *)&qp_start);
             if (NULL == (frame = ((afs) ? afs_get_video((OUTPUT_INFO *)oip, i_frame, &drop, next_jitter) : oip->func_get_video_ex(i_frame, aviutl_color_fmt)))) {
                 ret |= AUO_RESULT_ERROR; error_afs_get_frame();
                 break;
             }
+            QueryPerformanceCounter((LARGE_INTEGER *)&qp_end);
+            time_get_frame += (qp_end - qp_start) * qp_freq_sec;
 
             drop |= (afs & copy_frame);
 
@@ -1018,6 +1025,7 @@ static AUO_RESULT x265_out(CONF_GUIEX *conf, const OUTPUT_INFO *oip, PRM_ENC *pe
             write_log_auo_line_fmt(LOG_INFO, "drop %d / %d frames", pe->drop_count, i);
 
         write_log_auo_line_fmt(LOG_INFO, "CPU使用率: Aviutl: %.2f%% / x265: %.2f%%", GetProcessAvgCPUUsage(pe->h_p_aviutl, &time_aviutl), GetProcessAvgCPUUsage(pi_enc.hProcess));
+        write_log_auo_line_fmt(LOG_INFO, "Aviutl 平均フレーム取得時間: %.3f ms", time_get_frame * 1000.0 / i);
         write_log_auo_enc_time("x265エンコード時間", tm_vid_enc_fin - tm_vid_enc_start);
     }
 
