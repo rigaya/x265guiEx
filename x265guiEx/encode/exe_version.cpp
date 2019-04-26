a2a9404eb22d54559f5f1080c7690d7b7215fb53
rigaya <rigaya34589@live.jp>
2019-04-26 23:25:04 +0900

    c51580de でなおも不十分だったようなので修正。

--- x265guiEx/encode/exe_version.cpp
+++ x265guiEx/encode/exe_version.cpp
@@ -41,7 +41,7 @@ int get_x265ver_regex(const char *txt, int v[4]) {
     while (str.back() == '\r' || str.back() == '\n' || str.back() == ' ') {
         str.pop_back();
     }
-    std::regex re(R"((\d+).(\d+)(_[0-9A-Za-z]+)*(\+(\d+))?([-][A-Za-z0-9]+)*)"); // 正規表現
+    std::regex re(R"((\d+).(\d+)(_[0-9A-Za-z]+)*(\+(\d+))?(([-][A-Za-z0-9]+)+([ ].*)*)*)"); // 正規表現
     //matched:[0] = 3.12_Au+24, [1]=3, [2]=12, [3]=_Au, [4]=+24, [5]=24
     std::smatch match;
     int ret = 1;
