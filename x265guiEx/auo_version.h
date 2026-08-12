bf71b28f315c9dc23e96d524910f7c630b3829e9
rigaya <rigaya34589@live.jp>
2026-08-12 13:13:04 +0900

    同梱の実行ファイルを更新。

--- x265guiEx/auo_version.h
+++ x265guiEx/auo_version.h
@@ -28,9 +28,9 @@
 #ifndef _AUO_VERSION_H_
 #define _AUO_VERSION_H_
 
-#define AUO_VERSION          0,5,9,0
-#define AUO_VERSION_STR       "5.09"
-#define AUO_VERSION_STR_W    L"5.09"
+#define AUO_VERSION          0,5,10,0
+#define AUO_VERSION_STR       "5.10"
+#define AUO_VERSION_STR_W    L"5.10"
 #define AUO_NAME_R             x265guiEx
 #define AUO_NAME_WITHOUT_EXT  "x265guiEx"
 #define AUO_NAME_WITHOUT_EXT_W L"x265guiEx"
